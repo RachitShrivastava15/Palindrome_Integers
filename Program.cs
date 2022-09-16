8c3cef3496386bf0a9e285d5b9494f2e02d1540e
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-09-16 13:39:57 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace While
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = 1221;
+            int temp,remineder, sum = 0;
+            temp = number;
+            while (number > 0)
+            {
+                remineder = number % 10;
+                sum  = (sum *10) + remineder;
+                number = number / 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine($"{temp} is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine ($"{temp} is not Palindrome");
+            }
+        }
+    }
+}
